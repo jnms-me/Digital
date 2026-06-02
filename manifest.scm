5b5622a8dc56591a59a71196f0a3b4b8deb9b526
Jonas Meeuws <jonas.meeuws@gmail.com>
2026-06-02 22:23:58 +0200

    tools(guix): Add manifest.scm.
    * manifest.scm: New file.

---
+++ manifest.scm
@@ -0,0 +1,29 @@
+;; SPDX-License-Identifier: AGPL-3.0-only
+;; SPDX-FileCopyrightText: Copyright 2026 Jonas Meeuws
+(use-modules (guix channels)
+             (guix inferior)
+             (guix profiles)
+             (guix ui)
+             (srfi srfi-11)
+             (srfi srfi-26))
+
+(define (pkgs channels specs)
+  (let* ((inferior (inferior-for-channels channels))
+         (lookup (cut lookup-inferior-packages inferior <> <>)))
+    (map (lambda (spec)
+           (let-values (((name version output)
+                         (package-specification->name+version+output spec)))
+             (list (car (lookup name version))
+                   output)))
+         specs)))
+
+(packages->manifest
+ (pkgs (list (channel
+               (inherit %default-guix-channel)
+               (commit "f4d97180ea4fc468cb83e5ba35d9db392ea3a647")))
+       (list "bash"
+             "coreutils" "findutils" "sed" "grep" "which"
+             "openjdk@21:jdk" "maven"
+             ;;"java-cglib"
+             "git" "pre-commit"
+             "nss-certs")))
