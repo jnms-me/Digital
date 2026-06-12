3579f8ace0034d1cb19f9b79a589d5c58b6390f7
Jonas Meeuws <jonas.meeuws@gmail.com>
2026-06-12 16:33:17 +0200

    feat(simavr): Add ‘Simavr.Mcu’ enum.
    * src/main/java/de/neemann/digital/core/simavr/Simavr.java:
    (Mcu): New public inner enum.

--- src/main/java/de/neemann/digital/core/simavr/Simavr.java
+++ src/main/java/de/neemann/digital/core/simavr/Simavr.java
@@ -17,12 +17,79 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * The Simavr
  */
 public class Simavr extends Node implements Element {
     private static final Logger LOGGER = LoggerFactory.getLogger(Simavr.class);
+
+    public enum Mcu {
+        at90usb162    ("AT90usb162",              0, 8, 8, 8),
+        atmega48      ("ATmega48/48p/48pa",       0, 8, 7, 8),
+        atmega8       ("ATmega8/8l",              0, 8, 7, 8),
+        atmega88      ("ATmega88/88p/88pa",       0, 8, 7, 8),
+        atmega16      ("ATmega16",                0, 8, 7, 8),
+        atmega164     ("ATmega164/164p/164pa",    8, 8, 8, 8),
+        atmega168     ("ATmega168/168p/168pa",    0, 8, 7, 8),
+        atmega16m1    ("ATmega16m1",              0, 8, 8, 8, 3),
+        atmega32      ("ATmega32",                0, 8, 7, 8),
+        atmega324     ("ATmega324/324p",          8, 8, 8, 8),
+        atmega324a    ("ATmega324a/324pa",        8, 8, 8, 8),
+        atmega328     ("ATmega328/328p",          0, 8, 7, 8),
+        atmega328pb   ("ATmega328pb",             0, 8, 7, 8, 4),
+        atmega32u4    ("ATmega32u4",              8, 8, 8, 8, 8),
+        atmega644     ("ATmega644/644p",          0, 8, 8, 8, 8),
+        atmega64m1    ("ATmega64m1",              0, 8, 8, 8, 3),
+        atmega128     ("ATmega128/128L",          8, 8, 8, 8, 8, 8, 5),
+        atmega1280    ("ATmega1280",              0),
+        atmega1281    ("ATmega1281",              0),
+        atmega1284p   ("ATmega1284p/1284",        0),
+        atmega128rfa1 ("ATmega128rfa1",           0),
+        atmega128rfr2 ("ATmega128rfr2",           0),
+        atmega169p    ("ATmega169p",              8, 8, 8, 8, 8, 8, 6),
+        atmega2560    ("ATmega2560",              0),
+        attiny13      ("ATtiny13/attiny13a",      0),
+        attiny2313    ("ATtiny2313/attiny2313v",  0),
+        attiny2313a   ("ATtiny2313a",             0),
+        attiny24      ("ATtiny24",                0),
+        attiny25      ("ATtiny25",                0),
+        attiny4313    ("ATtiny4313",              0),
+        attiny44      ("ATtiny44",                0),
+        attiny45      ("ATtiny45",                0),
+        attiny84      ("ATtiny84",                0),
+        attiny85      ("ATtiny85",                0);
+
+        public static class Port {
+            public final char name;
+            public final int bits;
+
+            public Port(char name, int bits) {
+                this.name = name;
+                this.bits = bits;
+            }
+        }
+
+        public final String description;
+        public final List<Port> ports;
+
+        Mcu(String description, int... portsBits) {
+            this.description = description;
+            this.ports = IntStream.range(0, portsBits.length)
+                    .mapToObj(i -> {
+                        final char name = (char) ('A' + i);
+                        final int bits = portsBits[i];
+                        if (bits == 0)
+                            return null;
+                        return new Port(name, bits);
+                    })
+                    .filter(el -> el != null)
+                    .collect(Collectors.toUnmodifiableList());
+        }
+    }
 
     enum InputPin {
         PB0(0),
