8148ca91fddc35982c89d5fcfb020cecd0c1acde
Jonas Meeuws <jonas.meeuws@gmail.com>
2026-07-06 23:57:42 +0200

    feat(dig): Update ArduinoUno and example.
    * src/main/dig/simavr/ArduinoUno.dig: Redraw design, accounting for
    new simavr outputs.
    * src/main/dig/simavr/ArduinoUnoExample.dig: Adjust for new outputs.
    Add a break on ‘TxC’.
    * src/test/resources/simavr/test-firmware/firmware.c: Replace with an
    avr-libc hello world program.

--- src/test/resources/simavr/test-firmware/firmware.c
+++ src/test/resources/simavr/test-firmware/firmware.c
@@ -1,17 +1,40 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 // SPDX-FileCopyrightText: Copyright 2026 Jonas Meeuws
 #include <avr/io.h>
-#include <avr/sleep.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+static FILE uart0 = {};
+
+static int
+uart0_putc(char c,
+           FILE *stream)
+{
+  (void)stream;
+  while ((UCSR0A & _BV(UDRE0)) == 0) {}
+  UDR0 = c;
+  return 0;
+}
 
 int
 main()
 {
-  DDRB = 0x00;
-  DDRD = 0xFF;
-  while (1)
-    PORTD = PINB;
+  // Set B(5) to high (Arduino Uno internal LED).
+  DDRB  |= _BV(5);
+  PORTB |= _BV(5);
 
-  sleep_mode();
+  // Equivalent to setting the baud rate to F_CPU / 16.
+  UBRR0H = UBRR0L = 0;
+
+  fdev_setup_stream(&uart0, uart0_putc, NULL, _FDEV_SETUP_RW);
+  stdout = &uart0;
+
+  while (true)
+    {
+      fputs("Hello World!", stdout);
+      fflush(stdout);
+    }
+
   __builtin_unreachable();
 }
